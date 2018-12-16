0ff8da7934f01e18e3e3cb807c74d336f9cbdc07
jessestaff <36085683+jessestaff@users.noreply.github.com>
2018-12-16 15:07:17 -0500

    Add files via upload

---
+++ ballon-bot.py
@@ -0,0 +1,13 @@
+#Ballon bot by Jesssestaff#0010
+
+import discord
+from discord.ext import commands
+from discord.ext.commands import Bot
+import asyncio
+
+
+@bot.event
+async def on_ready():
+    print ("im ready bro")
+
+bot.run("")
