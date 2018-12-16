1e92af9a3e147b7235003c3be74f0f24fac16723
jessestaff <36085683+jessestaff@users.noreply.github.com>
2018-12-16 15:29:54 -0500

    Update ballon-bot.py

--- ballon-bot.py
+++ ballon-bot.py
@@ -5,9 +5,16 @@ from discord.ext import commands
 from discord.ext.commands import Bot
 import asyncio
 
+bot = commands.Bot(command_prefix='?')
 
 @bot.event
 async def on_ready():
     print ("im ready bro")
 
-bot.run("")
+@bot.command(pass_context=True)
+async def ping(ctx):
+        await bot.say("shut up faggot")
+        print ("user has been told to shut the hell up")
+    
+    
+bot.run("put id here")
